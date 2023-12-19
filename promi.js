eeed63cffc2f8ea1fc008bab8ee0b1a20678b9bd
Shivam12202945 <nowmailshivamkumar@gmail.com>
2023-12-19 14:49:15 +0530

    last commit

--- promi.js
+++ promi.js
@@ -8,5 +8,7 @@ promiseone.then(()=>{
     console.log("promise is fulfilled");
 }).catch(()=>{
     console.log("promise is rejected");
-});
+    console.log("this is the second commit");
+}); 
+console.log("here are the some changes for the second commit for this git");
 
